3f4844ca3cbdb6c3764640bc30cd4ad28c859ecb
Migle Beresineviciute <migle.brs@gmail.com>
2017-11-21 22:46:45 +0200

    Updated

--- main.go
+++ main.go
@@ -1,41 +1,16 @@
 package main
 
 import (
-    "log"
+	"log"
 	"net/http"
+	"fmt"
 )
 
 func main() {
 
+	fmt.Println("Connecting to database")
+	connection()
 	router := NewRouter()
-	log.Fatal(http.ListenAndServe(":8080", router))
-	// router := mux.NewRouter().StrictSlash(true)
-	// router.HandleFunc("/", MainHanlder)
-	// router.HandleFunc("/healthcheck", HealtchCheck)
-
-	// // independent 
-	// router.HandleFunc("/teams", teamsHandler)
-	// router.HandleFunc("/teams/{teamId}", teamHandler)
-	// router.HandleFunc("/cyclists", cyclistsHandler)
-	// router.HandleFunc("/cyclists/{cyclistId}", cyclistHandler)
-	// router.HandleFunc("/commissaires", commissairesHandler)
-	// router.HandleFunc("/commissaires/{comId}", commissaireHandler)
-	// router.HandleFunc("/managers", managersHandler)
-	// router.HandleFunc("/managers/{managerId}", managerHandler)
-
-	// // event dependence
-	// router.HandleFunc("/events", eventsHandler)
-	// router.HandleFunc("/events/{eventId}", eventHandler)
-	// router.HandleFunc("/event/{eventId}/participants", participantsHandler)
-	// router.HandleFunc("/event/{eventId}/participants/{participantId}", participantHandler)
-	// router.HandleFunc("/events/{eventId}/stages", stagesHandler)
-	// router.HandleFunc("/events/{eventId}/stages/{stageId}", stageHandler)
-	// router.HandleFunc("/events/{eventId}/stages/{stageId}/sprints", sprintsHandler)
-	// router.HandleFunc("/events/{eventId}/stages/{stageId}/sprints/{sprintId}", sprintHandler)
-	// router.HandleFunc("/events/{eventId}/racecommissaires", eventCommissairesHandler)
-	// router.HandleFunc("/events/{eventId}/racecommissaires/{commissaireId}", eventCommissaireHandler)
-	
-	
-    // log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":8025", router))
 }
 
